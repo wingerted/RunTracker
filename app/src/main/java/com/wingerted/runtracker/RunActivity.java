77659d143d98ecf7d41f0ebebd82995452f713f0
Winger <wingerted@gmail.com>
2014-10-07 22:19:37 +0800

    Create RunTracker Project

--- app/src/main/java/com/wingerted/runtracker/RunActivity.java
+++ app/src/main/java/com/wingerted/runtracker/RunActivity.java
@@ -1,39 +1,13 @@
 package com.wingerted.runtracker;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
 
 
-public class RunActivity extends Activity {
+public class RunActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_run);
+    protected Fragment createFragment() {
+        return new RunFragment();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.run, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
